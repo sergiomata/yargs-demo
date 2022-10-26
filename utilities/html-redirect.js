67667d4542dfc7f9b1f524d383f11a51233b17b3
sergiomata <32809578+sergiomata@users.noreply.github.com>
2022-10-25 19:20:48 -0500

    feat: add html-redirect

---
+++ utilities/html-redirect.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    console.log(window.location)
+    var newUrl =''
+    //*** referidos
+       if (window.location.hostname.includes('referidos.mymoons') || window.location.hostname.includes('referidos.moons.pe')){
+       	if (window.location.hostname === 'referidos.moons.pe'){
+          if (window.location.href.includes('?')){
+              var tempArray = window.location.href.split("?");
+              var additionalURL = tempArray[1];
+                newUrl = 'https://referidos.moonsdental.com/referidos/pe?'+tempArray[1]
+          }else{
+                newUrl = 'https://referidos.moonsdental.com/referidos/pe'
+          }
+        }else if (window.location.href.includes('referidos.mymoons')){
+          var hrefArray = window.location.hostname.split(".")
+          var suffix = hrefArray[2];
+          console.log({suffix})
+          if (window.location.href.includes('?')){
+              var tempArray = window.location.href.split("?");
+              var additionalURL = tempArray[1];
+                newUrl = 'https://referidos.moonsdental.com/referidos/'+ suffix +'?'+tempArray[1]
+          }else{
+                newUrl = 'https://referidos.moonsdental.com/referidos/'+ suffix
+          }
+        }
+         //else if (window.location.href ==='referidos.moonsdental.com'){
+        	//newUrl = 'https://referidos.moonsdental.com/referidos/mx'
+        //}
+         window.location.replace(newUrl)
+      }
+    
+    
+      //**** dudas.mymoons.mx 
+        if (window.location.hostname === 'dudas.mymoons.mx'){
+          if (window.location.href.includes('?')){
+            var tempArray = window.location.href.split("?");
+            var additionalURL = tempArray[1];
+              newUrl = 'https://dudas.moonsdental.com/dudas-mx'+'?'+additionalURL;
+
+            }else{
+              newUrl = 'https://dudas.moonsdental.com/dudas-mx'
+            }
+          window.location.replace(newUrl)
+        }
+  })
